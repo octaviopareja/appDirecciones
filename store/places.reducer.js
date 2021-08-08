250a48d1682fc371b54617f0d73dd365a0ff0453
Octavio Pareja <octaviopareja05@gmail.com>
2021-08-07 21:41:37 -0300

    fixes
    .

--- store/places.reducer.js
+++ store/places.reducer.js
@@ -1,38 +1,39 @@
-import { ADD_PLACE, LOAD_PLACES } from './places.action';
-import Place from  '../models/Place';
+import { ADD_PLACE, LOAD_PLACES } from "./places.action";
+import Place from "../models/Place";
 
 const initialState = {
-    places: []
-}
+  places: [],
+};
 
 export default (state = initialState, action) => {
-    switch(action.type) {
-        case ADD_PLACE:
-            const newPlace = new Place(
-                action.payload.id.toString(),
-                action.payload.title,
-                action.payload.image,
-                action.payload.address,
-                action.payload.coords.lat,
-                action.payload.coords.lng,
+  switch (action.type) {
+    case ADD_PLACE:
+      const newPlace = new Place(
+        action.payload.id.toString(),
+        action.payload.title,
+        action.payload.address,
+        action.payload.coords.lat,
+        action.payload.coords.lng
+      );
+      return {
+        ...state,
+        places: state.places.concat(newPlace),
+      };
+    case LOAD_PLACES:
+      return {
+        ...state,
+        places: action.places.map(
+          (item) =>
+            new Place(
+              item.id.toString(),
+              item.title,
+              item.address,
+              item.lat,
+              item.lng
             )
-            return {
-                ...state,
-                places: state.places.concat(newPlace),
-            };
-        case LOAD_PLACES:
-            return {
-                ...state,
-                places: action.places.map(item => new Place(
-                    item.id.toString(),
-                    item.title,
-                    item.image,
-                    item.address,
-                    item.lat,
-                    item.lng,
-                )),
-            }
-        default:
-            return state;
-    }
-}
+        ),
+      };
+    default:
+      return state;
+  }
+};
